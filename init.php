fe13fdc55862871dd3c14cfdb36620095bdc4870
damien-pilot-in <damien@pilot-in.com>
2020-05-25 17:43:55 +0200

    Add basic files

---
+++ init.php
@@ -0,0 +1,124 @@
+<?php
+
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * Get Pilo'Press path
+ *
+ * @return mixed
+ */
+function pip_path() {
+    return PIP_PATH;
+}
+
+/**
+ * Include if file exists
+ *
+ * @param string $filename
+ */
+function pip_include( $filename = '' ) {
+    $file_path = pip_path() . ltrim( $filename, '/' );
+    if ( file_exists( $file_path ) ) {
+        include_once( $file_path );
+    }
+}
+
+/**
+ * Load translation
+ *
+ * @param string $domain
+ *
+ * @return bool
+ */
+function pip_load_textdomain( $domain = 'pilopress' ) {
+    if ( !function_exists( 'acf_get_locale' ) ) {
+        return false;
+    }
+
+    $locale  = apply_filters( 'plugin_locale', acf_get_locale(), $domain );
+    $mo_file = $domain . '-' . $locale . '.mo';
+
+    // Try to load from the languages directory first.
+    if ( load_textdomain( $domain, WP_LANG_DIR . '/plugins/' . $mo_file ) ) {
+        return true;
+    }
+
+    // Load from plugin lang folder.
+    return load_textdomain( $domain, pip_path() . 'lang/' . $mo_file );
+}
+
+/**
+ * Enqueue Pilo'Press style
+ */
+function pip_enqueue() {
+    $tailwind_css = PIP_THEME_ASSETS_PATH . PIP_THEME_STYLE_FILENAME . '.min.css';
+
+    if ( file_exists( $tailwind_css ) ) {
+        wp_enqueue_style(
+            'style-pilopress',
+            PIP_THEME_ASSETS_URL . PIP_THEME_STYLE_FILENAME . '.min.css',
+            false
+        );
+    }
+}
+
+/**
+ * Enqueue Pilo'Press admin style
+ */
+function pip_enqueue_admin() {
+    $tailwind_admin_css = PIP_THEME_ASSETS_PATH . PIP_THEME_STYLE_ADMIN_FILENAME . '.min.css';
+
+    if ( file_exists( $tailwind_admin_css ) ) {
+        wp_enqueue_style(
+            'style-pilopress-admin',
+            PIP_THEME_ASSETS_URL . PIP_THEME_STYLE_ADMIN_FILENAME . '.min.css',
+            false
+        );
+    }
+}
+
+/**
+ * Check if ACF Pro and ACFE are activated
+ */
+add_action( 'after_plugin_row_' . PIP_BASENAME, 'pip_plugin_row', 5, 3 );
+function pip_plugin_row( $plugin_file, $plugin_data, $status ) {
+
+    // If ACF Pro and ACFE activated, return
+    if ( pilopress()->has_acf() && pilopress()->has_acfe() ) {
+        return;
+    }
+
+    ?>
+
+    <style>
+        .plugins tr[data-plugin='<?php echo PIP_BASENAME; ?>'] th,
+        .plugins tr[data-plugin='<?php echo PIP_BASENAME; ?>'] td {
+            box-shadow: none;
+        }
+
+        <?php if(isset($plugin_data['update']) && !empty($plugin_data['update'])){ ?>
+
+        .plugins tr.pilopress-plugin-tr td {
+            box-shadow: none !important;
+        }
+
+        .plugins tr.pilopress-plugin-tr .update-message {
+            margin-bottom: 0;
+        }
+
+        <?php } ?>
+    </style>
+
+    <tr class="plugin-update-tr active pilopress-plugin-tr">
+        <td colspan="3" class="plugin-update colspanchange">
+            <div class="update-message notice inline notice-error notice-alt">
+                <p><?php _e( 'Pilo\'Press requires Advanced Custom Fields PRO (minimum: 5.7.13) and ACF Extended.', 'pilopress' ); ?></p>
+            </div>
+        </td>
+    </tr>
+
+    <?php
+
+}
---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Pilo'Press - Pilot'In Pack ===
+Contributors: pilotin
+Donate link: https://www.pilot-in.com
+Tags: acf, page builder, tailwindcss
+Requires at least: 4.9
+Tested up to: 5.4.1
+Requires PHP: 5.6
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Quick start config we use at Pilot'in for WordPress & PiloPress
+
+== Description ==
+
+Quick start config we use at Pilot'in for WordPress & PiloPress
+
+== Requirements ==
+
+This plugin requires [Advanced Custom Fields PRO](https://www.advancedcustomfields.com/pro/) and [Advanced Custom Fields: Extended](https://wordpress.org/plugins/acf-extended/) and [Pilo'Press](https://wordpress.org/plugins/pilopress/) plugins in order to work correctly.
+
+== Getting started ==
+
+1. Activate Advanced Custom Fields Pro plugin
+2. Activate ACF Extended plugin
+3. Activate Pilo'Press plugin
+
+== Installation ==
+
+= Plugin Install =
+
+1. Activate Advanced Custom Fields Pro plugin
+2. Activate ACF Extended plugin
+3. Activate Pilo'Press plugin
+
+== Changelog ==
+
+= 0.1 - 14/05/2020 =
+* Initial commit
