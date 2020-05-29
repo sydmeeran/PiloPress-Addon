60af3e7c6afb0e44da797de9282889fe3f4dbb5f
maelysdarde <maelys@pilot-in.com>
2020-05-29 15:23:10 +0200

    refactor Bottom admin bar

--- includes/class-bottom-admin-bar.php
+++ includes/class-bottom-admin-bar.php
@@ -1,57 +1,62 @@
 <?php
-$BottomAdminBar = new PIP_Addon_Bottom_Admin_Bar();
 
-class PIP_Addon_Bottom_Admin_Bar {
-    public function __construct() {
-        add_action( 'after_setup_theme', array( &$this, 'show_toolbar_check' ) );
-        add_action( 'plugins_loaded', array( &$this, 'myplugin_init' ) );
-        add_action( 'wp_enqueue_scripts', array( &$this, 'admin_bar_script_init' ), 11 );
-        add_action( 'get_header', array( &$this, 'remove_admin_bar_css' ) );
-        add_action( 'wp_head', array( &$this, 'my_admin_bar_bump_cb' ) );
-        add_action( 'wp_footer', array( &$this, 'keyboard_shortcut' ), 21 );
-    }
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
 
-    /**
-     * Checking the 'Show Toolbar when viewing site' check box.
-     */
-    public function show_toolbar_check() {
-        wp_get_current_user();
-        if ( 'true' !== get_user_meta( get_current_user_id(), 'show_admin_bar_front', 1 ) ) {
-            return;
+if ( !class_exists( 'PIP_Addon_Bottom_Admin_Bar' ) ) {
+
+    class PIP_Addon_Bottom_Admin_Bar {
+        public function __construct() {
+            add_action( 'after_setup_theme', array( &$this, 'show_toolbar_check' ) );
+            add_action( 'plugins_loaded', array( &$this, 'myplugin_init' ) );
+            add_action( 'wp_enqueue_scripts', array( &$this, 'admin_bar_script_init' ), 11 );
+            add_action( 'get_header', array( &$this, 'remove_admin_bar_css' ) );
+            add_action( 'wp_head', array( &$this, 'my_admin_bar_bump_cb' ) );
+            add_action( 'wp_footer', array( &$this, 'keyboard_shortcut' ), 21 );
         }
-    }
 
-    /**
-     * Load plugin textdomain
-     */
-    public function myplugin_init() {
-        load_plugin_textdomain( 'bottom-admin-bar', false, dirname( plugin_basename( __FILE__ ) ) );
-    }
-
-    /**
-     * Override default admin bar CSS.
-     */
-    public function admin_bar_script_init() {
-        if ( is_user_logged_in() ) {
-            wp_register_style( 'adminBarStyleSheet', PIP_ADDON_URL . 'assets/css/view.css' );
-            wp_enqueue_style( 'adminBarStyleSheet' );
-            wp_enqueue_script( 'jquery' );
+        /**
+         * Checking the 'Show Toolbar when viewing site' check box.
+         */
+        public function show_toolbar_check() {
+            wp_get_current_user();
+            if ( 'true' !== get_user_meta( get_current_user_id(), 'show_admin_bar_front', 1 ) ) {
+                return;
+            }
         }
-    }
 
-    /**
-     * Remove default admin bar inline CSS
-     */
-    public function remove_admin_bar_css() {
-        remove_action( 'wp_head', '_admin_bar_bump_cb' );
-    }
+        /**
+         * Load plugin textdomain
+         */
+        public function myplugin_init() {
+            load_plugin_textdomain( 'bottom-admin-bar', false, dirname( plugin_basename( __FILE__ ) ) );
+        }
 
-    /**
-     * Rewrite admin bar inline CSS
-     */
-    public function my_admin_bar_bump_cb() {
-        $output = '<style type="text/css" media="screen">';
-        $output .= 'html { padding-bottom: 32px !important; }
+        /**
+         * Override default admin bar CSS.
+         */
+        public function admin_bar_script_init() {
+            if ( is_user_logged_in() ) {
+                wp_register_style( 'adminBarStyleSheet', PIP_ADDON_URL . 'assets/css/view.css' );
+                wp_enqueue_style( 'adminBarStyleSheet' );
+                wp_enqueue_script( 'jquery' );
+            }
+        }
+
+        /**
+         * Remove default admin bar inline CSS
+         */
+        public function remove_admin_bar_css() {
+            remove_action( 'wp_head', '_admin_bar_bump_cb' );
+        }
+
+        /**
+         * Rewrite admin bar inline CSS
+         */
+        public function my_admin_bar_bump_cb() {
+            $output = '<style type="text/css" media="screen">';
+            $output .= 'html { padding-bottom: 32px !important; }
             * html body { padding-bottom: 32px !important; }
             @media screen and ( max-width: 782px ) {
             html { padding-bottom: 46px !important; }
@@ -60,24 +65,24 @@ class PIP_Addon_Bottom_Admin_Bar {
             html.spaceClear { padding-bottom: 0 !important; }
             html.spaceClear body { padding-bottom: 0 !important;}';
 
-        // Delete Twenty Sixteen head spacing
-        if ( get_option( 'template' ) === 'twentysixteen' ) {
-            $output .= '.admin-bar:before {
+            // Delete Twenty Sixteen head spacing
+            if ( get_option( 'template' ) === 'twentysixteen' ) {
+                $output .= '.admin-bar:before {
                     top: 0 !important;
                 }';
+            }
+
+            $output .= '</style>';
+            if ( is_user_logged_in() ) {
+                echo $output;
+            }
         }
 
-        $output .= '</style>';
-        if ( is_user_logged_in() ) {
-            echo $output;
-        }
-    }
-
-    /**
-     * Add keyboard shortcut
-     */
-    public function keyboard_shortcut() {
-        $output = "<script type=\"text/javascript\">
+        /**
+         * Add keyboard shortcut
+         */
+        public function keyboard_shortcut() {
+            $output = "<script type=\"text/javascript\">
             jQuery( document ).ready( function( $ ){
                 $( 'body' ).keydown(function( event ){
                     if( event.shiftKey === true && event.which === 65 ){
@@ -88,8 +93,12 @@ class PIP_Addon_Bottom_Admin_Bar {
             });
             </script>";
 
-        if ( is_user_logged_in() ) {
-            echo $output;
+            if ( is_user_logged_in() ) {
+                echo $output;
+            }
         }
     }
+
+    // Instantiate
+    new PIP_Addon_Bottom_Admin_Bar();
 }
