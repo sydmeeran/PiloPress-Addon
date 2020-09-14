8e473cdd0ac57495fe48367d08b366c7be068bbe
damien-pilot-in <damien@pilot-in.com>
2020-09-14 18:16:04 +0200

    Add filter `pip_tax_post_type` for 3rd party to choose which archive post-type should be used 🚀

--- templates/taxonomy.php
+++ templates/taxonomy.php
@@ -17,6 +17,9 @@ if ( function_exists( 'the_pip_content' ) ) {
         $posts_type_related = pip_maybe_get( $current_taxo, 'object_type' );
         $post_type_related  = !empty( $posts_type_related ) ? reset( $posts_type_related ) : '';
 
+        // If 3rd party want to choose which archive post-type should be used
+        $post_type_related = apply_filters( 'pip_tax_post_type', $post_type_related, $posts_type_related, $current_taxo );
+
         switch ( $post_type_related ) {
             case 'post':
                 $archive_id = get_option( 'page_for_posts' );
